392c9bdd929e46c555e5437a76bed3eb3e7466e0
Hynek Zemanec <zemanech@yahoo.com>
2019-10-07 21:10:03 +0200

    adding support for code display

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,11 @@
-import 'storybook-addon-jsx/register'
-import '@storybook/addon-knobs/register';
+import "storybook-addon-jsx/register";
+import "@storybook/addon-knobs/register";
+
+// show SASS, HTML, TypeScript etc., more - https://github.com/SOFTVISION-University/storybook-addon-code
+import * as CodeAddon from "../src/register";
+
+CodeAddon.setTabs([
+  { label: "Sass", type: "sass" },
+  { label: "HTML", type: "html" },
+  { label: "JavaScript", type: "javascript" }
+]);
